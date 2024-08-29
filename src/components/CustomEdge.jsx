a7ab2c0e501feb60ef77e74a5a93ef9205dcce42
Ashutosh Joshi <aj875063@gmail.com>
2024-08-29 18:40:00 +0530

    children remove

--- src/components/CustomEdge.jsx
+++ src/components/CustomEdge.jsx
@@ -4,29 +4,30 @@ import { GiCancel } from "react-icons/gi";
 
 const CustomEdge = ({id,source,target,...props }) => {
     const [edgePath, labelX, labelY] = getBezierPath(props);
-    const {getNodes,setEdges}=useReactFlow();
+    const {getNodes,setEdges,setNodes}=useReactFlow();
     function edgehandler(){
         setEdges((prev)=>(
             prev.filter((edge)=>(
                 edge.id!==id
             ))
         ));
-        let parent;
-        getNodes().map((val)=>{
-            
-            if (val.id===source){
-                parent=val;
+        let arr=getNodes();
+        arr.forEach((val) => {
+            if (val.id === source) {
+                val.childNodes.forEach((edge,index)=>{
+                    if(edge.id===target){
+                        val.childNodes.splice(index,1);
+                    }
+                });
             }
-        })
-        parent.childNodes.filter((edge)=>(
-            edge.id!=target
-        ));
+        });
+        setNodes(arr);
     }
     return (
         <>
             <BaseEdge path={edgePath} />
             <EdgeLabelRenderer>
-                <div className="absolute p-2 rounded-md font-bold text-2xl text-red-500 pointer-events-auto"
+                <div className="absolute p-2 rounded-md font-bold text-2xl text-red-500 pointer-events-auto hover:cursor-pointer"
                 style={{transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,}}>
                     {<GiCancel onClick={edgehandler} />}
                 </div>
